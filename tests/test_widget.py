eaf6bec8537e4d29a8a01d03a9dc7175f60a1dc5
MaratNazmuhanov <nazmyx134@gmail.com>
2025-06-01 20:55:05 +0400

    fix test

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -3,8 +3,8 @@ from src.widget import mask_account_card, get_date
 
 def test_mask_account_card_cards():
     assert mask_account_card("Visa Platinum 7000792289606361") == "Visa Platinum 7000 79** **** 6361"
-    assert mask_account_card("MasterCard 7158300734726758") == "MasterCard 7158 **** **** 6758"
-    assert mask_account_card("Maestro 1596837868705199") == "Maestro 1596 **** **** 5199"
+    assert mask_account_card("MasterCard 7158300734726758") == "MasterCard 7158 30** **** 6758"
+    assert mask_account_card("Maestro 1596837868705199") == "Maestro 1596 83** **** 5199"
 
 def test_mask_account_card_schemes():
     assert mask_account_card("Счет 73654108430135874305") == "Счет **4305"
